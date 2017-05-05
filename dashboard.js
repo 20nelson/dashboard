548b0fb19c48d3efe6e2d40610c40931c086bb75
20nelson <20nelson@da.org>
2017-05-05 16:20:47 -0400

    fixes and additions

--- dashboard.js
+++ dashboard.js
@@ -1,21 +1,36 @@
-var ks = [];
-var vs = [];
 function readKey(k){
-  var keys = ks//NetworkTables.getValue("/dashboard/roboRIO/keys");
-  var values = vs//NetworkTables.getValue("/dashboard/roboRIO/values");
+
+  var keys = NetworkTables.getValue("/dashboard/roboRIO/keys");
+
+  var values = NetworkTables.getValue("/dashboard/roboRIO/values");
+
   return values[keys.indexOf(k)];
+
 }
 
+
 function writeKey(k, v){
-  var keys = ks//NetworkTables.getValue("/dashboard/driverStation/keys");
-  var values = vs//NetworkTables.getValue("/dashboard/driverStation/values");
+
+  var keys = NetworkTables.getValue("/dashboard/driverstation/keys");
+
+  var values = NetworkTables.getValue("/dashboard/driverstation/values");
+
   if(keys.indexOf(k) != -1){
+
     values[keys.indexOf(k)] = v;
+
   } else {
+
     console.log("key \'"+k+"\' does not exist. Adding...")
+
     keys.push(k);
+
     values.push(v);
+
   }
-  vs = values//NetworkTables.putValue("/dashboard/driverStation/keys", keys);
-  ks = keys//NetworkTables.putValue("/dashboard/driverStation/values", values);
+
+  NetworkTables.putValue("/dashboard/driverstation/keys", keys);
+
+  NetworkTables.putValue("/dashboard/driverstation/values", values);
+
 }
