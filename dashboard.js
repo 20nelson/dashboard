294f962bd3b3dfe0c28b44c79a98173fddc96d28
20nelson <20nelson@da.org>
2017-05-08 21:18:07 -0400

    added css and basic layout

--- dashboard.js
+++ dashboard.js
@@ -1,36 +1,18 @@
-function readKey(k){
-
-  var keys = NetworkTables.getValue("/dashboard/roboRIO/keys");
-
-  var values = NetworkTables.getValue("/dashboard/roboRIO/values");
-
-  return values[keys.indexOf(k)];
-
-}
-
-
-function writeKey(k, v){
-
-  var keys = NetworkTables.getValue("/dashboard/driverstation/keys");
-
-  var values = NetworkTables.getValue("/dashboard/driverstation/values");
-
-  if(keys.indexOf(k) != -1){
-
-    values[keys.indexOf(k)] = v;
-
-  } else {
-
-    console.log("key \'"+k+"\' does not exist. Adding...")
-
-    keys.push(k);
-
-    values.push(v);
-
-  }
-
-  NetworkTables.putValue("/dashboard/driverstation/keys", keys);
-
-  NetworkTables.putValue("/dashboard/driverstation/values", values);
-
+window.onload = function(){
+NetworkTables.addWsConnectionListener(function(connected){
+	if(connected){
+		document.getElementById('socketConnection').className = 'fa fa-lg fa-check';
+	} else {
+		document.getElementById('socketConnection').className = 'fa fa-lg fa-times';
+	}
+}, true);
+NetworkTables.addRobotConnectionListener(function(connected){
+	if(connected){
+		document.getElementById('robotConnection').className = 'fa fa-lg fa-check';
+	} else {
+		document.getElementById('robotConnection').className = 'fa fa-lg fa-times';
+		document.getElementById('fmsConnection').className = 'fa fa-lg fa-times';
+		// reset all values
+	}
+}, true);
 }
---
+++ tabledecoder.js
@@ -0,0 +1,34 @@
+function readKey(k){
+
+  var keys = NetworkTables.getValue("/dashboard/roboRIO/keys");
+  var values = NetworkTables.getValue("/dashboard/roboRIO/values");
+	if(keys.indexOf(k) == -1){
+		console.error("Key "+k+" does not exist!");
+		return undefined;
+	} else {
+  		return values[keys.indexOf(k)];
+	}
+}
+
+
+function writeKey(k, v){
+
+  var keys = NetworkTables.getValue("/dashboard/driverstation/keys");
+  var values = NetworkTables.getValue("/dashboard/driverstation/values");
+
+  if(keys.indexOf(k) !== -1){
+
+    values[keys.indexOf(k)] = v;
+
+  } else {
+
+    console.log("key \'"+k+"\' does not exist. Adding...");
+    keys.push(k);
+    values.push(v);
+
+  }
+
+  NetworkTables.putValue("/dashboard/driverstation/keys", keys);
+  NetworkTables.putValue("/dashboard/driverstation/values", values);
+
+}
