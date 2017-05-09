6d3fada16d250bd47d7e95ed0071f8f7241b237a
20nelson <20nelson@da.org>
2017-05-09 13:13:16 -0400

    fixed icon font and added launcher batch file

---
+++ dashboard.bat
@@ -0,0 +1,3 @@
+start C:/Users/frc/Documents/Dashboard/pynetworktables2js.exe --robot roboRIO-6502-FRC.local
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --app="http://localhost:8888"
+Exit
